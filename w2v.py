14622ff7d84fbec50597ef40fcdc31f53f65e578
dausion <dausion@vip.qq.com>
2018-06-27 09:33:25 +0800

    '7'

--- w2v.py
+++ w2v.py
@@ -38,9 +38,9 @@ from matplotlib.pylab import mpl
 dr = os.path.dirname(os.path.abspath(__file__))
 
 
-dr_out = os.path.join(dr,'output')
-os.makedirs(dr_out,0o777)
-dr_log = os.path.join(dr,'log')
+dr_out = 'output'
+os.mkdir(dr_out,0o777)
+dr_log = 'log'
 os.mkdir(dr_log,0o777)
 # Give a folder path as an argument with '--log_dir' to save
 # TensorBoard summaries. Default is a log folder in current directory.
